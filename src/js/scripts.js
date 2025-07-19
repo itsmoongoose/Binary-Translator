6a3d8b0c99f490dbe20fa5373494ac0ff50adb88
itsmoongoose <lewishlemay@gmail.com>
2025-07-18 19:54:03 -0600

    Reformat main function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 //Dictionary -- Binary & ASCII Characters
-/* const chart = {
+/* 
+const chart = {
     "00100001": "!",
+    ""
 
 }
 */
@@ -14,11 +16,29 @@
 //Function -- Main
 async function main() {
     do {
-        let binaryCode = await input("Please enter your binary code: ");
-        binaryCode = binaryCode.trim();
-        let binaryCodeArray = binaryCode.split(" ");
-        for (character of binaryCodeArray) {
+
+        //Menu
+        output("Choose one of the following: \na) Create Binary Numbers \nb) Translate Binary Code");
+        let menuChoice = await input("");
+
+        //Input Not Recognized
+        while (menuChoice != "a" || "b") {
+            menuChoice = await input("Sorry, I don't recognize what you're typing. Please try again: ");
+        }
+
+        //Creating Binary Number List
+        if (menuChoice == "a") {
             
+        }
+        
+        //Translating Binary Code
+        else if (menuChoice == "b") {
+            let binaryCode = await input("Please enter your binary code: ");
+            binaryCode = binaryCode.trim();
+            let binaryCodeArray = binaryCode.split(" ");
+            for (character of binaryCodeArray) {
+            
+            }
         }
 
 
