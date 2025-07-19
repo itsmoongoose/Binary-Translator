d3a6e609fddd3439e1ad9461c8dc9f77519d1c61
itsmoongoose <lewishlemay@gmail.com>
2025-07-18 19:43:43 -0600

    Add do while function and start conversion code/for loop

---
+++ src/js/scripts.js
@@ -0,0 +1,32 @@
+//Dictionary -- Binary & ASCII Characters
+/* const chart = {
+    "00100001": "!",
+
+}
+*/
+
+
+
+
+
+
+
+//Function -- Main
+async function main() {
+    do {
+        let binaryCode = await input("Please enter your binary code: ");
+        binaryCode = binaryCode.trim();
+        let binaryCodeArray = binaryCode.split(" ");
+        for (character of binaryCodeArray) {
+            
+        }
+
+
+
+
+
+
+
+    }
+    while (binaryCode) {}
+}
