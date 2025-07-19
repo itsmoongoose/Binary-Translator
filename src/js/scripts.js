632cc465110910da0b97e8e0ac7806dc9a6d041e
itsmoongoose <lewishlemay@gmail.com>
2025-07-18 20:56:47 -0600

    Fix bug causing reference error in while loop (menuChoice)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,11 +7,15 @@ const chart = {
 }
 */
 
+//Defining Variables
+let paddedNum = "";
+let startNum = 0;
+let menuChoice = "";
 
-
-
-
-
+//Function -- Padded Number
+function padding() {
+    paddedNum = String(startNum).padStart(8, '0');
+}
 
 //Function -- Main
 async function main() {
@@ -19,28 +23,25 @@ async function main() {
 
         //Menu
         output("Choose one of the following: \na) Create Binary Numbers \nb) Translate Binary Code");
-        let menuChoice = await input("");
+        menuChoice = await input("");
 
         //Input Not Recognized
-        while (menuChoice != "a" || "b") {
+        while (menuChoice != "a" && menuChoice != "b") {
             menuChoice = await input("Sorry, I don't recognize what you're typing. Please try again: ");
         }
 
         //Creating Binary Number List
         if (menuChoice == "a") {
-            let startNum = "00000000";
-            startNum = Number(startNum);
 
             //Listing Numbers
             for (let number = 0; number < 94; number = number + 1) {
-                let generatedNum = Number(generatedNum)
-                generatedNum = startNum + 1;
+                let generatedNum = startNum + 1
+                padding();
 
                 //Checking For "2"
-                let check = generatedNum.includes("2");
+                let check = paddedNum.includes("2");
                 if (check == true) {
                     generatedNum = startNum - 1;
-                    let generatedNumStr = String(generatedNum);
 
                     //Finding Last 0
                     lastOccur = str.lastIndexOf("0");
@@ -73,5 +74,5 @@ async function main() {
 
 
     }
-    while (menuChoice) {}
+    while (menuChoice === String) {}
 }
